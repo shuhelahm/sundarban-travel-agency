b20d7f128e3dd0df4a7131b28151a051f98b3cf9
shuhelahm <shuhel037@gmail.com>
2021-10-31 12:32:59 +0600

    updated

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://mydbuser1:lWazaARKV5HXiBFD@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
     try {
       await client.connect();
       const database = client.db("sundarban-travel-agency");
       const servicesCollection = database.collection("services");
+      console.log('database connected')
 
       // get api
       app.get('/services', async(req, res)=>{
